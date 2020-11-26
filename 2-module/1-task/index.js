d068d80fce7294daa68c0d00f1298c1b149e4477
Leonid Korsakov <KorsakovLA@mail.ru>
2020-11-26 08:45:18 +0300

    Первая задача 2го модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let totalSalary = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number') {
+      totalSalary += salaries[key];
+    }
+  }
+  return totalSalary;
 }
