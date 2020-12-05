095508e22b36150bc21cbb7ea2f628287ddc207e
Leonid Korsakov <KorsakovLA@mail.ru>
2020-12-05 11:27:52 +0300

    4 модуль вторая задача

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,10 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  let rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    let currentRow = rows[i];
+    let currentCells = [...currentRow.cells];
+    currentCells[i].style.backgroundColor = 'red';
+  }
 }
